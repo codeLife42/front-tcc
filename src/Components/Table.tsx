c20bcf250639a63284889b01cdc2b4f501ae2704
Tailson Borges de Andrade <tailsonandradesb@hotmail.com>
2024-06-09 11:21:30 -0300

    Atualizacao filtro carteira

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -5,22 +5,38 @@ import { getSinistroCliente } from "../servicos/sinistrocliente";
 export function Table() {
   const [sinistroCliente, setSinistroCliente] = useState<any[]>([]);
   const [carregando, setCarregando] = useState(true);
-  const [carteira, setCarteira] = useState("Plataforma PF");
+  const [carteira, setCarteira] = useState("Todas");
+  const [pesquisa, setPesquisa] = useState("");
+
+  const handleCarteiraChange = (event) => {
+    setCarteira(event.target.value);
+  };
+
+  const handlePesquisaChange = (event) => {
+    setPesquisa(event.target.value);
+  };
 
   useEffect(() => {
     fecthClientes();
   }, []);
 
+  //Busca os clientes no back-end
   async function fecthClientes() {
     const clientesFiltrados = await getSinistroCliente();
     setSinistroCliente(clientesFiltrados);
     setCarregando(false);
   }
 
-  console.log(sinistroCliente);
   if (carregando) {
     return <p>Carregando...</p>;
   }
+
+  const pesquisaCliente = sinistroCliente.filter((sinistro) => {
+    const nomeCliente = sinistro.nome.toLowerCase();
+    const barraPesquisa = pesquisa.toLocaleLowerCase();
+
+    return nomeCliente.includes(barraPesquisa);
+  });
 
   return (
     <div className={styles.positionTable}>
@@ -30,6 +46,8 @@ export function Table() {
           type="text"
           id="search-input"
           placeholder="Pesquisar cliente..."
+          value={pesquisa}
+          onChange={pesquisaCliente}
         />
         <button className={styles.searchButton} id="search-button">
           Pesquisar
@@ -42,14 +60,16 @@ export function Table() {
         <label>Nome</label>
       </div>
 
-      <select className={styles.customSelect} name="Carteiras">
-        <option value={carteira} onChange={() => setCarteira("Plataforma PF")}>
-          Plataforma
-        </option>
+      <select
+        className={styles.customSelect}
+        value={carteira}
+        name="Carteiras"
+        onChange={handleCarteiraChange}
+      >
+        <option value="Todas">Todas</option>
+        <option value="Plataforma PF">Plataforma</option>
         <option value="Carteira 01">Carteira 01</option>
-        <option value="Carteira 02" onChange={() => setCarteira("Carteira 02")}>
-          Carteira 02
-        </option>
+        <option value="Carteira 02">Carteira 02</option>
       </select>
       <table className={styles.styledTable}>
         <thead>
@@ -62,9 +82,20 @@ export function Table() {
         </thead>
         <tbody>
           {sinistroCliente.map((sinistro) => {
-            console.log(carteira);
-            console.log(sinistro.carteira);
             if (sinistro.carteira === carteira) {
+              console.log(carteira);
+              return (
+                <tr key={sinistro.id}>
+                  <td>{sinistro.cpf}</td>
+                  <td>{sinistro.carteira}</td>
+                  <td>{sinistro.nome}</td>
+                  <td>
+                    <a href="">{sinistro.status}</a>
+                  </td>
+                </tr>
+              );
+            }
+            if (carteira === "Todas") {
               return (
                 <tr key={sinistro.id}>
                   <td>{sinistro.cpf}</td>
